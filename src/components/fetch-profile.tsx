5190296f7e27436823099cf22ee1243dfe4e1066
Abdullah <abdullahbelal102938@gmail.com>
2025-11-19 10:16:02 +0200

    gggggg

--- src/components/fetch-profile.tsx
+++ src/components/fetch-profile.tsx
@@ -2,7 +2,7 @@
 import { useAppDispatch } from "@/store/hooks/dispatch";
 import { useAppSelector } from "@/store/hooks/selector";
 import { selectCurrentUserMainData, setUser } from "@/store/slices/user-slice";
-import { CLIENT_COLLECTOR_REQ, CURR_USER_PROFILE, REFRESH_TOKEN_REQ, setCookie } from "@/utils/requests/client-reqs/common-reqs";
+import { CLIENT_COLLECTOR_REQ, CURR_USER_PROFILE, setCookie } from "@/utils/requests/client-reqs/common-reqs";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect} from "react";
 
